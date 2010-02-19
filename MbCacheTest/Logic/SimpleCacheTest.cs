62f23dbe8f6b934451c99c8d07b1727295893356
roger.kratz <roger.kratz@localhost>
2010-02-19 14:52:13 +0000

    some missed files

---
+++ MbCacheTest/Logic/SimpleCacheTest.cs
@@ -0,0 +1,54 @@
+using System;
+using MbCache.Configuration;
+using MbCache.Core;
+using NUnit.Framework;
+
+namespace MbCacheTest.Logic
+{
+    [TestFixture]
+    public class SimpleCacheTest
+    {
+        private IMbCacheFactory factory;
+
+        [SetUp]
+        public void Setup()
+        {
+            var builder = new CacheBuilder();
+            builder.UseCacheFor<objectToCache>(c => c.CachedMethod());
+            factory = builder.BuildFactory();
+        }
+
+        [Test]
+        public void CacheWorks()
+        {
+            var obj = factory.Get<objectToCache>();
+
+            Assert.AreEqual(obj.CachedMethod(), obj.CachedMethod());
+            Assert.AreNotEqual(obj.NonCachedMethod(), obj.NonCachedMethod());
+        }
+
+        [Test]
+        public void OriginalTypeIsNotModified()
+        {
+            var obj = new objectToCache();
+
+            Assert.AreNotEqual(obj.CachedMethod(), obj.CachedMethod());
+            Assert.AreNotEqual(obj.NonCachedMethod(), obj.NonCachedMethod());
+        }
+    }
+
+    public class objectToCache
+    {
+        private Random r = new Random();
+
+        public virtual int CachedMethod()
+        {
+            return r.Next();
+        }
+
+        public virtual int NonCachedMethod()
+        {
+            return r.Next();
+        }
+    }
+}

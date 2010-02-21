a565924e07f6a00cd15267e17e6e89d45a905f13
roger.kratz <roger.kratz@localhost>
2010-02-21 15:04:30 +0000

    support to cache per interfaces instead of virtual methods

--- MbCacheTest/Logic/InterfaceCacheTest.cs
+++ MbCacheTest/Logic/InterfaceCacheTest.cs
@@ -1,5 +1,4 @@
-using System;
-using MbCache.Configuration;
+﻿using MbCache.Configuration;
 using MbCache.Core;
 using MbCacheTest.CacheForTest;
 using NUnit.Framework;
@@ -7,7 +6,7 @@ using NUnit.Framework;
 namespace MbCacheTest.Logic
 {
     [TestFixture]
-    public class SimpleCacheTest
+    public class InterfaceCacheTest
     {
         private IMbCacheFactory factory;
 
@@ -15,7 +14,7 @@ namespace MbCacheTest.Logic
         public void Setup()
         {
             var builder = new CacheBuilder();
-            builder.UseCacheForClass<ObjectReturningRandomNumbers>(c => c.CachedMethod());
+            builder.UseCacheForInterface<IObjectReturningNumbers, ObjectReturningNumbers>(c => c.CachedMethod());
 
             factory = builder.BuildFactory(new TestCacheFactory());
         }
@@ -23,7 +22,7 @@ namespace MbCacheTest.Logic
         [Test]
         public void CacheWorks()
         {
-            var obj = factory.Create<ObjectReturningRandomNumbers>();
+            var obj = factory.Create<IObjectReturningNumbers>();
 
             Assert.AreEqual(obj.CachedMethod(), obj.CachedMethod());
             Assert.AreNotEqual(obj.NonCachedMethod(), obj.NonCachedMethod());
@@ -32,26 +31,31 @@ namespace MbCacheTest.Logic
         [Test]
         public void OriginalTypeIsNotModified()
         {
-            var obj = new ObjectReturningRandomNumbers();
+            var obj = new ObjectReturningNumbers();
 
             Assert.AreNotEqual(obj.CachedMethod(), obj.CachedMethod());
             Assert.AreNotEqual(obj.NonCachedMethod(), obj.NonCachedMethod());
         }
+    }
 
+    public interface IObjectReturningNumbers
+    {
+        int CachedMethod();
+        int NonCachedMethod();
+    }
 
-        public class ObjectReturningRandomNumbers
+    public class ObjectReturningNumbers : IObjectReturningNumbers
+    {
+        private int value = 0;
+
+        public int CachedMethod()
         {
-            private readonly Random r = new Random();
+            return value++;
+        }
 
-            public virtual int CachedMethod()
-            {
-                return r.Next();
-            }
-
-            public virtual int NonCachedMethod()
-            {
-                return r.Next();
-            }
+        public int NonCachedMethod()
+        {
+            return value++;
         }
     }
 }

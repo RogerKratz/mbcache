b60a78de415b0318a6278f3a76d79c420e6667d0
roger.kratz <roger.kratz@localhost>
2010-02-22 12:19:16 +0000

    invalidate one method

--- MbCache/Core/IMbCacheFactory.cs
+++ MbCache/Core/IMbCacheFactory.cs
@@ -1,8 +1,12 @@
+using System;
+using System.Linq.Expressions;
+
 namespace MbCache.Core
 {
     public interface IMbCacheFactory
     {
         T Create<T>() where T : class;
         void Invalidate<T>();
+        void Invalidate<T>(Expression<Func<T, object>> method);
     }
 }
---
+++ MbCache/Logic/ExpressionHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace MbCache.Logic
+{
+    internal static class ExpressionHelper
+    {
+        internal static string MemberName(Expression expression)
+        {
+            switch (expression.NodeType)
+            {
+                case ExpressionType.MemberAccess:
+                    var memberExpression = (MemberExpression)expression;
+                    var supername = MemberName(memberExpression.Expression);
+
+                    if (String.IsNullOrEmpty(supername))
+                        return memberExpression.Member.Name;
+
+                    return String.Concat(supername, '.', memberExpression.Member.Name);
+
+                case ExpressionType.Call:
+                    var callExpression = (MethodCallExpression)expression;
+                    return callExpression.Method.Name;
+
+                case ExpressionType.Convert:
+                    var unaryExpression = (UnaryExpression)expression;
+                    return MemberName(unaryExpression.Operand);
+
+                case ExpressionType.Parameter:
+                    return String.Empty;
+
+                default:
+                    throw new ArgumentException("The expression is not a member access or method call expression");
+            }
+        }
+    }
+}

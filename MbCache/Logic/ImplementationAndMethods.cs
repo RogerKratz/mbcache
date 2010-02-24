6640445f8753e37bbf1fa38cdd3e17e45ad8ac7c
roger.kratz <roger.kratz@localhost>
2010-02-24 11:10:43 +0000

    now interface caching is supported

--- MbCache/Logic/ImplementationAndMethods.cs
+++ MbCache/Logic/ImplementationAndMethods.cs
@@ -5,13 +5,15 @@ namespace MbCache.Logic
 {
     public class ImplementationAndMethods
     {
-        public ImplementationAndMethods(object[] ctorParams)
+        public ImplementationAndMethods(object[] ctorParams, object implementation)
         {
             Methods = new HashSet<MethodInfo>();
             CtorParameters = ctorParams;
+            Implementation = implementation;
         }
 
         public ICollection<MethodInfo> Methods { get; private set; }
         public object[] CtorParameters { get; private set; }
+        public object Implementation { get; private set; }
     }
 }

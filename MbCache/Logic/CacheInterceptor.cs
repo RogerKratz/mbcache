41824729d48d55e5480746f445c36c8f1292d4e3
roger.kratz <roger.kratz@localhost>
2010-02-25 12:38:07 +0000

    starting to use log4net. Used in CacheInterceptor now.

--- MbCache/Logic/CacheInterceptor.cs
+++ MbCache/Logic/CacheInterceptor.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Reflection;
 using Castle.Core.Interceptor;
+using log4net;
 
 namespace MbCache.Logic
 {
     public class CacheInterceptor : IInterceptor, ICacheableSignatures
     {
+        private static ILog log = LogManager.GetLogger(typeof (CacheInterceptor));
+
         private readonly ICache _cache;
         private readonly IMbCacheRegion _cacheRegion;
         private readonly Type _orgType;
@@ -23,24 +26,31 @@ namespace MbCache.Logic
 
         public void Intercept(IInvocation invocation)
         {
-            if(methodIsCached(invocation.Method))
+            var method = invocation.Method;
+            var typeAndMethodName = "<" + _orgType + "." + method.Name + "()>";
+            if(methodIsCached(method))
             {
-                var method = invocation.Method;
+                log.Debug("Intercepting " + typeAndMethodName + " using mbcache");
                 var typeAndMethodKey = _cacheRegion.Region(_orgType, method);
                 var key = typeAndMethodKey + _cacheRegion.AdditionalRegionsForParameterValues(_orgType, method, invocation.Arguments);
-                object cachedValue = _cache.Get(key);
-                if(cachedValue!=null)
+                log.Debug("Trying to find cache entry <" + key +"> for " + typeAndMethodName);
+                var cachedValue = _cache.Get(key);
+                if (cachedValue != null)
                 {
+                    log.Debug("Cache hit for " + typeAndMethodName);
                     invocation.ReturnValue = cachedValue;
                 }
                 else
                 {
+                    log.Debug("Cache miss for " + typeAndMethodName);
                     invocation.Proceed();
+                    log.Debug("Put in cache entry <" + key + "> for " + typeAndMethodName);
                     _cache.Put(key, invocation.ReturnValue);
                 }
             }
             else
             {
+                log.Debug("Intercepting method " + typeAndMethodName + " but skip it");
                 invocation.Proceed();                
             }
         }

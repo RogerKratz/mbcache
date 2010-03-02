f898e5762a05b0a1ccca88f2d797f34832fc3e0a
roger.kratz <roger.kratz@localhost>
2010-03-02 14:04:36 +0000

    some comments and clean up

--- MbCache/Logic/ICache.cs
+++ MbCache/Logic/ICache.cs
@@ -1,5 +1,12 @@
 namespace MbCache.Logic
 {
+    /// <summary>
+    /// Object communicating with 3rd part cache framework
+    /// </summary>
+    /// <remarks>
+    /// Created by: rogerkr
+    /// Created date: 2010-03-02
+    /// </remarks>
     public interface ICache
     {
         object Get(string key);

3118599abe2731d20609b327038db80158dbdc04
roger.kratz <roger.kratz@localhost>
2010-05-26 16:19:08 +0000

    rename of test

--- MbCacheTest/Logic/CachingMethodsWithParametersTest.cs
+++ MbCacheTest/Logic/CachingMethodsWithParametersTest.cs
@@ -7,7 +7,7 @@ using NUnit.Framework;
 
 namespace MbCacheTest.Logic
 {
-    public class CachedMethodHasToStringParametersTest
+    public class CachingMethodsWithParametersTest
     {
         private IMbCacheFactory factory;
 

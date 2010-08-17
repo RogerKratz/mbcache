e7229fa0d3aa89de12cd91b09beca80f154b5ef6
roger.kratz <roger.kratz@localhost>
2010-08-17 13:56:25 +0000

    setting version on AssemblyInfo

--- MbCache.ProxyGenerator.Castle/Properties/AssemblyInfo.cs
+++ MbCache.ProxyGenerator.Castle/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MbCache.ProxyImpl.Castle")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Licensed under LPGL")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.*")]
+[assembly: AssemblyFileVersion("0.6.0.0")]

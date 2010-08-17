10f08c767678bc25235fd08e945d2b87684033aa
roger.kratz <roger.kratz@localhost>
2010-08-17 13:41:21 +0000

    fixes issue 4

--- MbCache/Core/IMbCacheFactory.cs
+++ MbCache/Core/IMbCacheFactory.cs
@@ -16,8 +16,37 @@ namespace MbCache.Core
     /// </remarks>
     public interface IMbCacheFactory
     {
+        /// <summary>
+        /// Creates the proxy with ctor parameters.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Created by: rogerkr
+        /// Created date: 2010-08-17
+        /// </remarks>
         T Create<T>(params object[] parameters);
+
+        /// <summary>
+        /// Invalidates all cache entrys for instance T.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <remarks>
+        /// Created by: rogerkr
+        /// Created date: 2010-08-17
+        /// </remarks>
         void Invalidate<T>();
+
+        /// <summary>
+        /// Invalidates all cache entrys for method T.method.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="method">The method.</param>
+        /// <remarks>
+        /// Created by: rogerkr
+        /// Created date: 2010-08-17
+        /// </remarks>
         void Invalidate<T>(Expression<Func<T, object>> method);
     }
 }

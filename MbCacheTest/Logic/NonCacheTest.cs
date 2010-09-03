f2bd47c58cd560767cbbbec06e3b54a41e75e8f3
roger.kratz <roger.kratz@localhost>
2010-09-03 12:52:15 +0000

    Added IMbCacheFactory.KnownInstance + test + impl

---
+++ MbCacheTest/Logic/NonCacheTest.cs
@@ -0,0 +1,36 @@
+using MbCache.Configuration;
+using MbCache.Core;
+using MbCache.DefaultImpl;
+using MbCacheTest.CacheForTest;
+using MbCacheTest.TestData;
+using NUnit.Framework;
+
+namespace MbCacheTest.Logic
+{
+    [TestFixture]
+    public class NonCacheTest
+    {
+        private IMbCacheFactory factory;
+
+        [SetUp]
+        public void Setup()
+        {
+            var builder = new CacheBuilder(ConfigurationData.ProxyImpl, new TestCacheFactory(), new ToStringMbCacheKey());
+
+            builder
+                .For<ObjectReturningNewGuids>()
+                .CacheMethod(c => c.CachedMethod())
+                .CacheMethod(c => c.CachedMethod2())
+                .As<IObjectReturningNewGuids>();
+
+            factory = builder.BuildFactory();
+        }
+
+        [Test]
+        public void CanAskFactoryIfComponentIsKnownType()
+        {
+            Assert.IsTrue(factory.KnownInstance(factory.Create<IObjectReturningNewGuids>()));
+            Assert.IsFalse(factory.KnownInstance(new object()));
+        }
+    }
+}

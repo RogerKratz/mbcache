90bb711821b17ad6f0f5c9c5314457f63ba87e8d
roger.kratz <roger.kratz@localhost>
2010-09-25 12:58:45 +0000

    comments

--- MbCache/Core/IStatistics.cs
+++ MbCache/Core/IStatistics.cs
@@ -2,8 +2,19 @@ namespace MbCache.Core
 {
     public interface IStatistics
     {
+        /// <summary>
+        /// Clear statistics
+        /// </summary>
         void Clear();
+
+        /// <summary>
+        /// Number of cache hits
+        /// </summary>
         long CacheHits { get; }
+
+        /// <summary>
+        /// Number of cache misses
+        /// </summary>
         long CacheMisses { get; }
     }
 }

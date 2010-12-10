156ce200c63644b0b3c4271cf2aac737d4b75b9a
roger kratz <roger.kratz@gmail.com>
2010-12-10 09:30:05 +0000

    simpler concurrency handling in LogAndStatisticCacheDecorator. using interlocked instead of lock()

--- MbCache/Logic/LogAndStatisticCacheDecorator.cs
+++ MbCache/Logic/LogAndStatisticCacheDecorator.cs
@@ -1,79 +1,75 @@
+using System.Threading;
 using log4net;
 using MbCache.Configuration;
 using MbCache.Core;
 
 namespace MbCache.Logic
 {
-    public class LogAndStatisticCacheDecorator : ICache, IStatistics
-    {
-        private ILog log;
-        private readonly ICache _cache;
-        private readonly object cacheMissLocker = new object();
-        private readonly object cacheHitLocker = new object();
+	public class LogAndStatisticCacheDecorator : ICache, IStatistics
+	{
+		private ILog log;
+		private readonly ICache _cache;
+		private long _cacheHits;
+		private long _cacheMisses;
 
-        public LogAndStatisticCacheDecorator(ICache cache)
-        {
-            _cache = cache;
-            log = LogManager.GetLogger(cache.GetType());
-        }
+		public LogAndStatisticCacheDecorator(ICache cache)
+		{
+			_cache = cache;
+			log = LogManager.GetLogger(cache.GetType());
+		}
 
-        public long CacheHits { get; private set; }
-        public long CacheMisses { get; private set; }
+		public long CacheHits
+		{
+			get { return _cacheHits; }
+		}
 
-        public object Get(string key)
-        {
-            log.Debug("Trying to find cache entry <" + key + ">");
-            var ret = _cache.Get(key);
-            if(ret == null)
-            {
-                cacheMiss(key);
-            }
-            else
-            {
-                cacheHit(key);
-            }
-            return ret;
-        }
+		public long CacheMisses
+		{
+			get { return _cacheMisses; }
+		}
 
-        public void Put(string key, object value)
-        {
-            log.Debug("Put in cache entry <" + key + ">");
-            _cache.Put(key, value);
-        }
+		public object Get(string key)
+		{
+			log.Debug("Trying to find cache entry <" + key + ">");
+			var ret = _cache.Get(key);
+			if(ret == null)
+			{
+				cacheMiss(key);
+			}
+			else
+			{
+				cacheHit(key);
+			}
+			return ret;
+		}
 
-        public void Delete(string keyStartingWith)
-        {
-            _cache.Delete(keyStartingWith);
-        }
+		public void Put(string key, object value)
+		{
+			log.Debug("Put in cache entry <" + key + ">");
+			_cache.Put(key, value);
+		}
 
-        public void Clear()
-        {
-            lock(cacheMissLocker)
-            {
-                CacheMisses = 0;                
-            }
-            lock (cacheHitLocker)
-            {
-                CacheHits = 0;
-            }
-        }
+		public void Delete(string keyStartingWith)
+		{
+			_cache.Delete(keyStartingWith);
+		}
 
-        private void cacheHit(string key)
-        {
-            log.Debug("Cache hit for <" + key + ">");
-            lock (cacheHitLocker)
-            {
-                CacheHits++;
-            }
-        }
+		public void Clear()
+		{
+			_cacheMisses = 0;                
+			_cacheHits = 0;
+		}
 
-        private void cacheMiss(string key)
-        {
-            log.Debug("Cache miss for <" + key + ">");
-            lock (cacheMissLocker)
-            {
-                CacheMisses++;
-            }
-        }
-    }
+		private void cacheHit(string key)
+		{
+			log.Debug("Cache hit for <" + key + ">");
+			Interlocked.Increment(ref _cacheHits);
+		}
+
+		private void cacheMiss(string key)
+		{
+			log.Debug("Cache miss for <" + key + ">");
+			Interlocked.Increment(ref _cacheMisses);
+		}
+	}
 }

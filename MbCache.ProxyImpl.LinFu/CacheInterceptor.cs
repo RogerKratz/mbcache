dff2b9f27c0e5215d3b24d2bc771854882dac9ef
roger kratz <roger.kratz@gmail.com>
2012-01-20 10:19:42 +0000

    Can now decide to not use caching in runtime based on parameter values. If IMbCacheKey's methods return null, mbcache isn't used.

--- MbCache.ProxyImpl.LinFu/CacheInterceptor.cs
+++ MbCache.ProxyImpl.LinFu/CacheInterceptor.cs
@@ -7,87 +7,93 @@ using MbCache.Logic;
 
 namespace MbCache.ProxyImpl.LinFu
 {
-    public class CacheInterceptor : IInterceptor
-    {
-        private readonly ICache _cache;
-        private readonly IMbCacheKey _cacheKey;
-        private readonly Type _type;
-        private readonly ImplementationAndMethods _methodData;
-        private readonly object _target;
-        private readonly ICachingComponent _cachingComponent;
+	public class CacheInterceptor : IInterceptor
+	{
+		private readonly ICache _cache;
+		private readonly IMbCacheKey _cacheKey;
+		private readonly Type _type;
+		private readonly ImplementationAndMethods _methodData;
+		private readonly object _target;
+		private readonly ICachingComponent _cachingComponent;
 
-        public CacheInterceptor(ICache cache, 
-                                IMbCacheKey cacheKey, 
-                                Type type,
-                                ImplementationAndMethods methodData,
-                                params object[] ctorParameters) 
-        {
-            _cache = cache;
-            _cacheKey = cacheKey;
-            _type = type;
-            _methodData = methodData;
-            _target = createTarget(ctorParameters);
-            _cachingComponent = new CachingComponent(cache, cacheKey, type, methodData);
-        }
+		public CacheInterceptor(ICache cache,
+										IMbCacheKey cacheKey,
+										Type type,
+										ImplementationAndMethods methodData,
+										params object[] ctorParameters)
+		{
+			_cache = cache;
+			_cacheKey = cacheKey;
+			_type = type;
+			_methodData = methodData;
+			_target = createTarget(ctorParameters);
+			_cachingComponent = new CachingComponent(cache, cacheKey, type, methodData);
+		}
 
-        private object createTarget(object[] ctorParameters)
-        {
-            try
-            {
-                return Activator.CreateInstance(_methodData.ConcreteType, ctorParameters);
-            }
-            catch (MissingMethodException ex)
-            {
-                var ctorParamMessage = "Incorrect number of parameters to ctor for type " + _methodData.ConcreteType;
-                throw new ArgumentException(ctorParamMessage, ex);
-            }
-        }
+		private object createTarget(object[] ctorParameters)
+		{
+			try
+			{
+				return Activator.CreateInstance(_methodData.ConcreteType, ctorParameters);
+			}
+			catch (MissingMethodException ex)
+			{
+				var ctorParamMessage = "Incorrect number of parameters to ctor for type " + _methodData.ConcreteType;
+				throw new ArgumentException(ctorParamMessage, ex);
+			}
+		}
 
-        public object Intercept(InvocationInfo info)
-        {
-            return methodMarkedForCaching(info.TargetMethod) ? 
-                    interceptUsingCache(info) : callOriginalMethod(info);
-        }
+		public object Intercept(InvocationInfo info)
+		{
+			return methodMarkedForCaching(info.TargetMethod) ?
+					  interceptUsingCache(info) : callOriginalMethod(info);
+		}
 
-        private bool methodMarkedForCaching(MethodInfo method)
-        {
-            //why?
-            foreach (var cachedMethod in _methodData.Methods)
-            {
-                if (cachedMethod.Name.Equals(method.Name))
-                    return true;
-            }
-            return false;
-        }
+		private bool methodMarkedForCaching(MethodInfo method)
+		{
+			//why?
+			foreach (var cachedMethod in _methodData.Methods)
+			{
+				if (cachedMethod.Name.Equals(method.Name))
+					return true;
+			}
+			return false;
+		}
 
-        private object interceptUsingCache(InvocationInfo info)
-        {
-            object retVal;
-            var method = info.TargetMethod;
-            var arguments = info.Arguments;
-            var key = _cacheKey.Key(_type, _cachingComponent, method, arguments);
+		private object interceptUsingCache(InvocationInfo info)
+		{
+			object retVal;
+			var method = info.TargetMethod;
+			var arguments = info.Arguments;
+			var key = _cacheKey.Key(_type, _cachingComponent, method, arguments);
+			if (key == null)
+			{
+				retVal = callOriginalMethod(info);
+			}
+			else
+			{
+				var cachedValue = _cache.Get(key);
+				if (cachedValue != null)
+				{
+					retVal = cachedValue;
+				}
+				else
+				{
+					retVal = callOriginalMethod(info);
+					_cache.Put(key, retVal);
+				}				
+			}
 
-            var cachedValue = _cache.Get(key);
-            if (cachedValue != null)
-            {
-                retVal = cachedValue;
-            }
-            else
-            {
-                retVal = callOriginalMethod(info);
-                _cache.Put(key, retVal);
-            }
+			return retVal;
+		}
 
-            return retVal;
-        }
-
-        private object callOriginalMethod(InvocationInfo info)
-        {
-            if(info.TargetMethod.DeclaringType.Equals(typeof(ICachingComponent)))
-            {
-                return info.TargetMethod.Invoke(_cachingComponent, info.Arguments);
-            }
-            return info.TargetMethod.Invoke(_target, info.Arguments);
-        }
-    }
+		private object callOriginalMethod(InvocationInfo info)
+		{
+			if (info.TargetMethod.DeclaringType == typeof(ICachingComponent))
+			{
+				return info.TargetMethod.Invoke(_cachingComponent, info.Arguments);
+			}
+			return info.TargetMethod.Invoke(_target, info.Arguments);
+		}
+	}
 }
--- MbCache/Configuration/MbCacheKeyBase.cs
+++ MbCache/Configuration/MbCacheKeyBase.cs
@@ -6,78 +6,75 @@ using MbCache.Core;
 
 namespace MbCache.Configuration
 {
-    /// <summary>
-    /// Base class for users to override to implement
-    /// their own logic for building cache keys
-    /// 
-    /// Will build cache key in format
-    /// MbCache|Type|Component|Method|ParamValue1|ParamValue2
-    /// </summary>
-    /// <remarks>
-    /// Created by: rogerkr
-    /// Created date: 2010-03-02
-    /// </remarks>
-    public abstract class MbCacheKeyBase : IMbCacheKey
-    {
-        public string Key(Type type)
-        {
-            return string.Concat(KeyStart + type + Separator);
-        }
+	/// <summary>
+	/// Base class for users to override to implement
+	/// their own logic for building cache keys
+	/// 
+	/// Will build cache key in format
+	/// MbCache|Type|Component|Method|ParamValue1|ParamValue2
+	/// </summary>
+	/// <remarks>
+	/// Created by: rogerkr
+	/// Created date: 2010-03-02
+	/// </remarks>
+	public abstract class MbCacheKeyBase : IMbCacheKey
+	{
+		public string Key(Type type)
+		{
+			return string.Concat(KeyStart + type + Separator);
+		}
 
-        public string Key(Type type, ICachingComponent component)
-        {
-            return string.Concat(Key(type), component.UniqueId, Separator);
-        }
+		public string Key(Type type, ICachingComponent component)
+		{
+			return string.Concat(Key(type), component.UniqueId, Separator);
+		}
 
-        public string Key(Type type, ICachingComponent component, MethodInfo method)
-        {
-            var ret = new StringBuilder(Key(type, component));
-            ret.Append(method.Name);
-            ret.Append(Separator);
-            foreach (var parameter in method.GetParameters())
-            {
-                ret.Append(parameter.Name);
-                ret.Append(Separator);
-            }
-            return ret.ToString();
-        }
+		public string Key(Type type, ICachingComponent component, MethodInfo method)
+		{
+			var ret = new StringBuilder(Key(type, component));
+			ret.Append(method.Name);
+			ret.Append(Separator);
+			foreach (var parameter in method.GetParameters())
+			{
+				ret.Append(parameter.Name);
+				ret.Append(Separator);
+			}
+			return ret.ToString();
+		}
 
-        public string Key(Type type, ICachingComponent component, MethodInfo method, IEnumerable<object> parameters)
-        {
-            var ret = new StringBuilder(Key(type, component, method));
-            foreach (var parameter in parameters)
-            {
-                ret.Append(parameter == null ? NullKey : ParameterValue(parameter));
-                ret.Append(Separator);
-            }
-            return ret.ToString();  
-        }
+		public string Key(Type type, ICachingComponent component, MethodInfo method, IEnumerable<object> parameters)
+		{
+			var ret = new StringBuilder(Key(type, component, method));
+			foreach (var parameter in parameters)
+			{
+				var parameterKey = ParameterValue(parameter);
+				if (parameterKey == null)
+					return null;
+				ret.Append(parameterKey);
+				ret.Append(Separator);
+			}
+			return ret.ToString();
+		}
 
 
-        protected virtual string Separator
-        {
-            get { return "|"; }
-        }
+		protected virtual string Separator
+		{
+			get { return "|"; }
+		}
 
-        protected virtual string KeyStart
-        {
-            get { return "MbCache" + Separator; }
-        }
+		protected virtual string KeyStart
+		{
+			get { return "MbCache" + Separator; }
+		}
 
-        protected virtual string NullKey
-        {
-            get { return "Null"; }
-        }
 
-        /// <summary>
-        /// Adds string to cache key for parameter values  
-        /// </summary>
-        /// <param name="parameter">The parameter.</param>
-        /// <returns></returns>
-        /// <remarks>
-        /// Created by: rogerkr
-        /// Created date: 2010-09-02
-        /// </remarks>
-        protected abstract string ParameterValue(object parameter);
-    }
+		/// <summary>
+		/// Adds string to cache key for parameter values  
+		/// </summary>
+		/// <param name="parameter">The parameter.</param>
+		/// <returns>
+		/// A string reporesentation of the parameter.
+		/// </returns>
+		protected abstract string ParameterValue(object parameter);
+	}
 }

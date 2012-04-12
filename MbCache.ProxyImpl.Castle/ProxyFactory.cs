ddf5ef660a77f26820450b1470bb5ed60c94d9da
roger kratz <roger.kratz@gmail.com>
2012-04-12 20:26:16 +0000

    Removing checked in libs, using nuget instead. Upgrading Castle.Core.dll to 3.0

--- MbCache.ProxyImpl.Castle/ProxyFactory.cs
+++ MbCache.ProxyImpl.Castle/ProxyFactory.cs
@@ -6,37 +6,43 @@ using MbCache.Logic;
 
 namespace MbCache.ProxyImpl.Castle
 {
-    public class ProxyFactory : IProxyFactory
-    {
-        private static readonly ProxyGenerator _generator = new ProxyGenerator(new DefaultProxyBuilder(new ModuleScope(false, true)));
-        private ICache _cache;
-        private IMbCacheKey _mbCacheKey;
+	public class ProxyFactory : IProxyFactory
+	{
+		private static readonly ProxyGenerator _generator = new ProxyGenerator(new DefaultProxyBuilder(new ModuleScope(false, true)));
+		private ICache _cache;
+		private IMbCacheKey _mbCacheKey;
 
-        public void Initialize(ICache cache, IMbCacheKey mbCacheKey)
-        {
-            _cache = cache;
-            _mbCacheKey = mbCacheKey;
-        }
+		public void Initialize(ICache cache, IMbCacheKey mbCacheKey)
+		{
+			_cache = cache;
+			_mbCacheKey = mbCacheKey;
+		}
 
-        public T CreateProxy<T>(ImplementationAndMethods methodData,
-                                params object[] parameters)
-        {
-            var type = typeof(T);
-            var cacheInterceptor = new CacheInterceptor(_cache, _mbCacheKey, type);
-            var options = new ProxyGenerationOptions(new CacheProxyGenerationHook(methodData.Methods));
-            options.AddMixinInstance(createCachingComponent(type, methodData));
-            return (T)_generator.CreateClassProxy(methodData.ConcreteType, options, parameters, cacheInterceptor);
+		public T CreateProxy<T>(ImplementationAndMethods methodData,
+										params object[] parameters)
+		{
+			var type = typeof(T);
+			var cacheInterceptor = new CacheInterceptor(_cache, _mbCacheKey, type);
+			var options = new ProxyGenerationOptions(new CacheProxyGenerationHook(methodData.Methods));
+			options.AddMixinInstance(createCachingComponent(type, methodData));
+			try
+			{
+				return (T)_generator.CreateClassProxy(methodData.ConcreteType, options, parameters, cacheInterceptor);
+			}
+			catch (InvalidProxyConstructorArgumentsException castleEx)
+			{
+				throw new ArgumentException("Cannot instantiate proxy", castleEx);
+			}
+		}
 
-        }
+		public bool AllowNonVirtualMember
+		{
+			get { return true; }
+		}
 
-        public bool AllowNonVirtualMember
-        {
-            get { return true; }
-        }
-
-        private ICachingComponent createCachingComponent(Type type, ImplementationAndMethods details)
-        {
-            return new CachingComponent(_cache, _mbCacheKey, type, details);
-        }
-    }
+		private ICachingComponent createCachingComponent(Type type, ImplementationAndMethods details)
+		{
+			return new CachingComponent(_cache, _mbCacheKey, type, details);
+		}
+	}
 }

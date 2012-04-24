aaf58be79f660d28de0c3dd0fc7d8a1668b72908
Roger Kratz <roger.kratz@gmail.com>
2012-04-24 14:02:19 +0200

    adding SimpleTest to enable test not running twice

---
+++ MbCacheTest/SimpleTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using MbCache.Configuration;
+using MbCacheTest.CacheForTest;
+using NUnit.Framework;
+
+namespace MbCacheTest
+{
+	public abstract class SimpleTest
+	{
+		protected SimpleTest(): this("MbCacheTest.ProxyImplThatThrows") 
+		{
+		}
+
+		protected SimpleTest(string proxyTypeString)
+		{
+			var proxyType = Type.GetType(proxyTypeString, true);
+			ProxyFactory = (IProxyFactory) Activator.CreateInstance(proxyType);
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			CacheBuilder = new CacheBuilder(ProxyFactory, CreateCache(), CreateCacheKey());
+			TestSetup();
+		}
+
+		protected virtual void TestSetup(){}
+
+		protected CacheBuilder CacheBuilder { get; private set; }
+		protected IProxyFactory ProxyFactory { get; private set; }
+
+		protected virtual ICache CreateCache()
+		{
+			return new TestCache();
+		}
+
+		protected virtual IMbCacheKey CreateCacheKey()
+		{
+			return new ToStringMbCacheKey();
+		}
+	}
+}

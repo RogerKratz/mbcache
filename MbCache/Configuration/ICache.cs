d2c9d8d3ac256d3704a115ba2164cc0f4857462f
Roger Kratz <roger.kratz@gmail.com>
2012-05-01 11:16:23 +0200

    minor

--- MbCache/Configuration/ICache.cs
+++ MbCache/Configuration/ICache.cs
@@ -3,10 +3,6 @@
 	/// <summary>
 	/// Object communicating with 3rd part cache framework
 	/// </summary>
-	/// <remarks>
-	/// Created by: rogerkr
-	/// Created date: 2010-03-02
-	/// </remarks>
 	public interface ICache
 	{
 		object Get(string key);

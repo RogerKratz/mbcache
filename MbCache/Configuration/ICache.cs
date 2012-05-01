0ba0df7c506f1ae1156b9621db2260cb3146ce8d
Roger Kratz <roger.kratz@gmail.com>
2012-05-01 11:20:07 +0200

    some xml comments to ICache

--- MbCache/Configuration/ICache.cs
+++ MbCache/Configuration/ICache.cs
@@ -5,9 +5,30 @@
 	/// </summary>
 	public interface ICache
 	{
+		/// <summary>
+		/// Gets the cached object.
+		/// </summary>
+		/// <param name="key">The key of this cache entry.</param>
 		object Get(string key);
+
+		/// <summary>
+		/// Puts <paramref name="value"/> to the cache.
+		/// </summary>
+		/// <param name="key">The key of this cache entry.</param>
+		/// <param name="value">The object to cache.</param>
 		void Put(string key, object value);
+
+		/// <summary>
+		/// Deletes all cache entries starting with <paramref name="keyStartingWith"/>.
+		/// </summary>
+		/// <param name="keyStartingWith">The key to search for.</param>
 		void Delete(string keyStartingWith);
+
+		/// <summary>
+		/// An implementation of <see cref="ILockObjectGenerator"/>.
+		/// This only needs to be set if two simultanious threads must
+		/// not modify cache at the same time.
+		/// </summary>
 		ILockObjectGenerator LockObjectGenerator { get; }
 	}
 }

316260cc0ae92c565b4fe6b333654ea894fee204
Roger Kratz <roger.kratz@gmail.com>
2012-05-15 15:10:31 +0200

    issue 10 fixed
    Added .net 4 test project.

---
+++ MbCacheNet4Test/readme.txt
@@ -0,0 +1,2 @@
+﻿Specific test for .net 4.
+Later, when MbCache is upgraded to new .net version, this project can be removed.
--- MbCacheTest/FullTest.cs
+++ MbCacheTest/FullTest.cs
@@ -2,7 +2,6 @@
 
 namespace MbCacheTest
 {
-
 	[TestFixture("MbCache.ProxyImpl.Castle.ProxyFactory, MbCache.ProxyImpl.Castle")]
 	[TestFixture("MbCache.ProxyImpl.LinFu.ProxyFactory, MbCache.ProxyImpl.LinFu")]
 	public abstract class FullTest : SimpleTest

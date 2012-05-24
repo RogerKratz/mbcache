72aee5bf585220e1d29fe952816466c917ee4937
roger kratz <roger.kratz@gmail.com>
2012-05-24 21:38:47 +0000

    minor

--- MbCache/Configuration/ILockObjectGenerator.cs
+++ MbCache/Configuration/ILockObjectGenerator.cs
@@ -11,7 +11,6 @@
 		/// Two non identical <paramref name="key"/> can return same instance,
 		/// but that could result in more locking than needed.
 		/// </summary>
-		/// <param name="key"></param>
 		/// <returns>Lock object for the key. Returns null if no locking should occur</returns>
 		object GetFor(string key);
 	}

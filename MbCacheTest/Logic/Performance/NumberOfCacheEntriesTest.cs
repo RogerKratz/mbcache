33c64c597eb16e4233d4556961a11d7e8042832b
Roger Kratz <roger.kratz@gmail.com>
2013-03-18 15:01:39 +0100

    failing test for issue 20

---
+++ MbCacheTest/Logic/Performance/NumberOfCacheEntriesTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Caching;
+using MbCacheTest.TestData;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace MbCacheTest.Logic.Performance
+{
+	public class NumberOfCacheEntriesTest : FullTest
+	{
+		private ObjectWithMultipleParameters component;
+		private MemoryCache memoryCacheReference;
+
+		public NumberOfCacheEntriesTest(string proxyTypeString) : base(proxyTypeString)
+		{
+		}
+
+		protected override void TestSetup()
+		{
+			memoryCacheReference = MemoryCache.Default;
+			CacheBuilder.For<ObjectWithMultipleParameters>()
+				.CacheMethod(x => x.Calculate(0, null, 0))
+				.AsImplemented();
+			component = CacheBuilder.BuildFactory().Create<ObjectWithMultipleParameters>();
+		}
+
+		[Test]
+		public void ShouldOnlyCreateOneCacheEntryForMultipleParameters()
+		{
+			component.Calculate(1, "1", 1);
+			var noOfCacheItemsBefore = memoryCacheReference.GetCount();
+			component.Calculate(2, "2", 2);
+			(memoryCacheReference.GetCount() - noOfCacheItemsBefore).Should().Be.EqualTo(1);
+		}
+
+		[Test]
+		public void ShouldNotCreateCacheEntryForSameParameters()
+		{
+			component.Calculate(1, "1", 1);
+			var noOfCacheItemsBefore = memoryCacheReference.GetCount();
+			component.Calculate(1, "1", 1);
+			(memoryCacheReference.GetCount() - noOfCacheItemsBefore).Should().Be.EqualTo(0);
+		}
+	}
+}
---
+++ MbCacheTest/TestData/ObjectWithMultipleParameters.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MbCacheTest.TestData
+{
+	public class ObjectWithMultipleParameters
+	{
+		public virtual Guid Calculate(int a, string b, long c)
+		{
+			return Guid.NewGuid();
+		}
+	}
+}

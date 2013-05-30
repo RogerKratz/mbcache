aef2572346e6af942621494de538515dd114f980
Roger Kratz <roger.kratz@gmail.com>
2013-05-30 16:27:01 +0200

    finishing issue 22

--- MbCacheTest/Logic/EnableDisable/DisableTest.cs
+++ MbCacheTest/Logic/EnableDisable/DisableTest.cs
@@ -1,4 +1,5 @@
-﻿using MbCache.Core;
+﻿using System;
+using MbCache.Core;
 using MbCacheTest.TestData;
 using NUnit.Framework;
 using SharpTestsEx;
@@ -45,6 +46,44 @@ namespace MbCacheTest.Logic.EnableDisable
 			factory.DisableCache<IObjectReturningNewGuids>(false);
 			var newRes = comp.CachedMethod();
 			comp.CachedMethod().Should().Not.Be.EqualTo(newRes);
+			factory.EnableCache<IObjectReturningNewGuids>();
+			comp.CachedMethod().Should().Be.EqualTo(orgRes);
+		}
+
+		[Test]
+		public void ShouldThrowIfEnablingNonComponentType()
+		{
+			Assert.Throws<ArgumentException>(() => factory.EnableCache<DisableTest>());
+		}
+
+		[Test]
+		public void ShouldThrowIfDisablingNonComponentType()
+		{
+			Assert.Throws<ArgumentException>(() => factory.DisableCache<DisableTest>());
+		}
+
+		[Test]
+		public void CanEnableMultipleTimes()
+		{
+			var comp = factory.Create<IObjectReturningNewGuids>();
+			var orgRes = comp.CachedMethod();
+			factory.EnableCache<IObjectReturningNewGuids>();
+			factory.EnableCache<IObjectReturningNewGuids>();
+			factory.EnableCache<IObjectReturningNewGuids>();
+			factory.EnableCache<IObjectReturningNewGuids>();
+			comp.CachedMethod().Should().Be.EqualTo(orgRes);
+		}
+
+
+		[Test]
+		public void CanDisableMultipleTimes()
+		{
+			var comp = factory.Create<IObjectReturningNewGuids>();
+			var orgRes = comp.CachedMethod();
+			factory.DisableCache<IObjectReturningNewGuids>(false);
+			factory.DisableCache<IObjectReturningNewGuids>(false);
+			factory.DisableCache<IObjectReturningNewGuids>(false);
+			factory.DisableCache<IObjectReturningNewGuids>(false);
 			factory.EnableCache<IObjectReturningNewGuids>();
 			comp.CachedMethod().Should().Be.EqualTo(orgRes);
 		}

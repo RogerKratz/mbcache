373ecbabee5088882dc6962fe7dd9ccc9cb943a4
Roger Kratz <roger.kratz@gmail.com>
2013-12-09 14:47:03 +0100

    Perf test for issue 23

---
+++ MbCacheTest/Logic/Performance/MemoryUsageTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MbCacheTest.TestData;
+using NUnit.Framework;
+
+namespace MbCacheTest.Logic.Performance
+{
+	[TestFixture]
+	public class MemoryUsageTest : FullTest
+	{
+		private ObjectWithParameterReturningFixedValue component;
+
+		public MemoryUsageTest(string proxyTypeString) : base(proxyTypeString)
+		{
+		}
+
+		protected override void TestSetup()
+		{
+			CacheBuilder.For<ObjectWithParameterReturningFixedValue>()
+				.CacheMethod(x => x.DoIt(0))
+				.AsImplemented();
+			component = CacheBuilder.BuildFactory().Create<ObjectWithParameterReturningFixedValue>();
+		}
+
+		[Test, Explicit]
+		public void ShouldLowerMemoryUsageCausedByKeyString()
+		{
+			const int uniqueCacheEntries = 10000;
+			var memUsageAtStart = GC.GetTotalMemory(true);
+			using (new NoLogger())
+			{
+				for (var i = 0; i < uniqueCacheEntries; i++)
+				{
+					component.DoIt(i);
+				}				
+			}
+			var memUsage = GC.GetTotalMemory(true) - memUsageAtStart;
+			Console.WriteLine(memUsage / 1000 / 1000 + "mb");
+		}
+	}
+}

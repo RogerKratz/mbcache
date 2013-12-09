901616427a371bd1788047c0e9a71a3073951b6f
Roger Kratz <roger.kratz@gmail.com>
2013-12-09 14:47:03 +0100

    Perf test for issue 23

---
+++ MbCacheTest/NoLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using log4net;
+using log4net.Core;
+
+namespace MbCacheTest
+{
+	public class NoLogger : IDisposable
+	{
+		private readonly Level levelBefore;
+
+		public NoLogger()
+		{
+			var repo = LogManager.GetRepository();
+			levelBefore = repo.Threshold;
+			repo.Threshold = Level.Off;
+		}
+
+		public void Dispose()
+		{
+			LogManager.GetRepository().Threshold = levelBefore;
+		}
+	}
+}

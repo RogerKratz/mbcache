f9cb6be69cb18046c91c4e37d3c9c861eb6f2d2c
Roger Kratz <roger.kratz@gmail.com>
2015-09-01 09:43:28 +0200

    Adding test to measure cache hit performance

---
+++ MbCacheTest/Logic/Performance/CacheHitPerformanceTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using log4net;
+using MbCacheTest.TestData;
+using NUnit.Framework;
+
+namespace MbCacheTest.Logic.Performance
+{
+	public class CacheHitPerformanceTest : FullTest
+	{
+		private IReturningRandomNumbers instance;
+
+		public CacheHitPerformanceTest(string proxyTypeString) : base(proxyTypeString)
+		{
+		}
+
+		protected override void TestSetup()
+		{
+			LogManager.Shutdown(); 
+			CacheBuilder.For<ReturningRandomNumbers>()
+				 .CacheMethod(c => c.CachedNumber())
+				 .As<IReturningRandomNumbers>();
+
+			var factory = CacheBuilder.BuildFactory();
+			instance = factory.Create<IReturningRandomNumbers>();
+		}
+
+		[TearDown]
+		public void AfterTest()
+		{
+			SetupFixtureForAssembly.StartLog4Net();
+		}
+
+		[Test]
+		public void MeasureCacheHitPerf()
+		{
+			const int loops = 300000;
+			instance.CachedNumber();
+			var stopwatch = new Stopwatch();
+			stopwatch.Start();
+			for (var i = 0; i < loops; i++)
+			{
+				instance.CachedNumber();
+			}
+			stopwatch.Stop();
+			Console.WriteLine(stopwatch.Elapsed);
+			//On my computer 
+			//~0.7s, castle
+			//~1.4s, linfu
+		}
+	}
+}

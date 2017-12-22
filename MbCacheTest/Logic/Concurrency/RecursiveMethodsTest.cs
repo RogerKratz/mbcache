1bf6334bb5b4b5619308cb21dedc9a0514fa0c1a
Roger Kratz <roger.kratz@gmail.com>
2017-12-22 11:02:31 +0100

    Adding test to ensure no deadlocks

---
+++ MbCacheTest/Logic/Concurrency/RecursiveMethodsTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MbCacheTest.TestData;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace MbCacheTest.Logic.Concurrency
+{
+	public class RecursiveMethodsTest : FullTest
+	{
+		public RecursiveMethodsTest(Type proxyType) : base(proxyType)
+		{
+		}
+		
+		[Test]
+		public void ShouldHandleRecursiveCalls()
+		{
+			CacheBuilder.For<ObjectRecursive1>()
+				.CacheMethod(c => c.Ref(1))
+				.AsImplemented();
+			CacheBuilder.For<ObjectRecursive2>()
+				.CacheMethod(c => c.Foo(1))
+				.AsImplemented();
+
+			var factory = CacheBuilder.BuildFactory();
+			var instance2 = factory.Create<ObjectRecursive2>();
+			var instance1 = factory.Create<ObjectRecursive1>(instance2);
+			
+			var tasks = new List<Task>();
+			
+			1000.Times(x =>
+			{
+				tasks.Add(Task.Factory.StartNew(() =>
+				{
+					instance1.Ref(x);
+					instance2.Foo(x);
+				}));
+				tasks.Add(Task.Factory.StartNew(() =>
+				{
+					instance2.Foo(x);
+					instance1.Ref(x);
+				}));
+			});
+			
+			Task.WaitAll(tasks.ToArray());
+		}
+	}
+}
---
+++ MbCacheTest/TestData/ObjectRecursive.cs
@@ -0,0 +1,25 @@
+﻿namespace MbCacheTest.TestData
+{
+	public class ObjectRecursive1
+	{
+		private readonly ObjectRecursive2 _objectRecursive2;
+
+		public ObjectRecursive1(ObjectRecursive2 objectRecursive2)
+		{
+			_objectRecursive2 = objectRecursive2;
+		}
+		
+		public virtual int Ref(int a)
+		{
+			return _objectRecursive2.Foo(a);
+		}
+	}
+
+	public class ObjectRecursive2
+	{
+		public virtual int Foo(int a)
+		{
+			return a;
+		}
+	}
+}

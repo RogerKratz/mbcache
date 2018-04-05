fc73394d0a2e33ab5c1ffe6f238d9b41f73a97ea
Roger Kratz <roger.kratz@gmail.com>
2018-04-05 13:00:20 +0200

    Ignored red test CachedMethodCalledFromWithinSameClassTest

---
+++ MbCacheTest/Logic/CachedMethodCalledFromWithinSameClassTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MbCache.Core;
+using MbCacheTest.TestData;
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace MbCacheTest.Logic
+{
+	[Ignore("Should be fixed")]
+	public class CachedMethodCalledFromWithinSameClassTest : FullTest
+	{
+		private IMbCacheFactory factory;
+
+		
+		public CachedMethodCalledFromWithinSameClassTest(Type proxyType) : base(proxyType)
+		{
+		}
+
+		protected override void TestSetup()
+		{
+			CacheBuilder.For<ObjectCallingMethodOnItSelf>()
+				.CacheMethod(c => c.CachedMethod())
+				.As<IObjectCallingMethodOnItSelf>();
+
+			factory = CacheBuilder.BuildFactory();
+		}
+
+		[Test]
+		public void VerifyNestedMethodWorks()
+		{
+			var obj1 = factory.Create<IObjectCallingMethodOnItSelf>();
+			var obj2 = factory.Create<IObjectCallingMethodOnItSelf>();
+			
+			obj1.NonCachedMethodCallingCachedMethod()
+				.Should().Be.EqualTo(obj2.NonCachedMethodCallingCachedMethod());
+		}
+	}
+}
---
+++ MbCacheTest/TestData/ObjectCallingMethodOnItSelf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MbCacheTest.TestData
+{
+	public interface IObjectCallingMethodOnItSelf
+	{
+		Guid NonCachedMethodCallingCachedMethod();
+	}
+	
+	public class ObjectCallingMethodOnItSelf : IObjectCallingMethodOnItSelf
+	{
+		public Guid NonCachedMethodCallingCachedMethod()
+		{
+			return CachedMethod();
+		}
+
+		public virtual Guid CachedMethod()
+		{
+			return Guid.NewGuid();
+		}
+	}
+}

828c0c803546f6dcc4588cfde6c2915d85301599
Roger Kratz <roger.kratz@gmail.com>
2019-08-06 15:39:08 +0200

    adding test ensuring memory usage after invalidate. good to have if/when unique locks are added for each entry

--- MbCacheTest/Logic/Performance/MemoryUsageTest.cs
+++ MbCacheTest/Logic/Performance/MemoryUsageTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MbCache.Core;
 using MbCacheTest.TestData;
 using NUnit.Framework;
 using SharpTestsEx;
@@ -8,6 +9,7 @@ namespace MbCacheTest.Logic.Performance
 	public class MemoryUsageTest : TestCase
 	{
 		private ObjectWithMultipleParameters component;
+		private IMbCacheFactory factory;
 
 		public MemoryUsageTest(Type proxyType) : base(proxyType)
 		{
@@ -18,7 +20,8 @@ namespace MbCacheTest.Logic.Performance
 			CacheBuilder.For<ObjectWithMultipleParameters>()
 				.CacheMethod(x => x.Calculate(0, "", 0))
 				.AsImplemented();
-			component = CacheBuilder.BuildFactory().Create<ObjectWithMultipleParameters>();
+			factory = CacheBuilder.BuildFactory();
+			component = factory.Create<ObjectWithMultipleParameters>();
 		}
 
 		[Test]
@@ -32,9 +35,26 @@ namespace MbCacheTest.Logic.Performance
 				component.Calculate(i, "", i);
 			}
 			var memUsage = GC.GetTotalMemory(true) - memUsageAtStart;
-			var mbUsage = memUsage / 1000 / 1000;
+			var mbUsage = memUsage / 1024 / 1024;
 			Console.WriteLine(mbUsage + "mb");
-			mbUsage.Should().Be.LessThan(21);
+			mbUsage.Should().Be.LessThan(20);
+		}
+
+		[Test]
+		public void MeasureMemAfterClear()
+		{
+			const int uniqueCacheEntries = 10000;
+			var memUsageAtStart = GC.GetTotalMemory(true);
+
+			for (var i = 0; i < uniqueCacheEntries; i++)
+			{
+				component.Calculate(i, "", i);
+			}
+			factory.Invalidate(component);
+			var memUsage = GC.GetTotalMemory(true) - memUsageAtStart;
+			var mbUsage = memUsage / 1024 / 1024;
+			Console.WriteLine(mbUsage + "mb");
+			mbUsage.Should().Be.LessThan(2);
 		}
 	}
 }

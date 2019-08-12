4c932f189a1667835a4bb78cdc7d88f9191f4840
Roger Kratz <roger.kratz@gmail.com>
2019-08-12 15:42:19 +0200

    Updating build scripts

--- Build/createRelease.bat
+++ Build/createRelease.bat
@@ -1,32 +1,16 @@
 @echo off
-SET ROOTDIR=%~dp0
-set ROOTDIR=%ROOTDIR:~0,-7%
-set msbuild="%PROGRAMFILES(X86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe"
-set configuration=Release
-set nugetFolder="%ROOTDIR%\.nuget"
-set PackageFolder="%ROOTDIR%\packages"
-set msbuildtasksVersion=1.4.0.65
 
 echo Before creating a release, remember to...
 echo * Update release notes
 echo.
 
-echo Installing msbuildtasks to %PackageFolder%. Please wait...
-%nugetFolder%\NuGet install MsBuildTasks -o %PackageFolder% -Version %msbuildtasksVersion%
-
 echo.
-set /p Version=Please enter version number, eg 1.2.0.0: 
-set /p NugetVersion=Please enter nuget version number, eg 1.2: 
+set /p Version=Please enter version number, eg 4.1.0: 
 
-%msbuild% default.msbuild /v:q /t:MakeRelease
-
-git checkout -- %ROOTDIR%\MbCache.ProxyImpl.LinFu\Properties\AssemblyInfo.cs
-git checkout -- %ROOTDIR%\MbCache\Properties\AssemblyInfo.cs
-git checkout -- %ROOTDIR%\MbCacheTest\Properties\AssemblyInfo.cs
+dotnet msbuild default.msbuild /t:MakeRelease
 
 echo -------------------------------
 echo.
-echo Updated assemblyinfo files to %Version%.
 echo Created a new nuget package to output folder.
 echo Did NOT push it to nuget.org
 echo.

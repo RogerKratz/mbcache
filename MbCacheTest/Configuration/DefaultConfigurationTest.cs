dcc92b4fddbc2299dffd7b4ab9abc2b239d401b9
Roger Kratz <roger.kratz@gmail.com>
2025-09-22 14:53:09 +0200

    simplifying test now when we only have one IProxyFactory impl in test suite

--- MbCacheTest/Configuration/DefaultConfigurationTest.cs
+++ MbCacheTest/Configuration/DefaultConfigurationTest.cs
@@ -1,27 +1,18 @@
-﻿using System;
-using MbCache.Configuration;
+﻿using MbCache.Configuration;
+using MbCache.ProxyImpl.LinFu;
 using MbCacheTest.TestData;
 using NUnit.Framework;
 using SharpTestsEx;
 
 namespace MbCacheTest.Configuration
 {
-	[TestFixture("MbCache.ProxyImpl.LinFu.LinFuProxyFactory, MbCache.ProxyImpl.LinFu")]
 	public class DefaultConfigurationTest
 	{
-		private readonly IProxyFactory _proxyFactory;
-
-		public DefaultConfigurationTest(string proxyTypeString)
-		{
-			var proxyType = Type.GetType(proxyTypeString, true);
-			_proxyFactory = (IProxyFactory)Activator.CreateInstance(proxyType);
-		}
-
 		[Test] 
 		public void ShouldCache()
 		{
 			using (var factory = new CacheBuilder()
-				.SetProxyFactory(_proxyFactory)
+				.SetProxyFactory(new LinFuProxyFactory())
 				.For<ObjectReturningNewGuids>()
 				.CacheMethod(c => c.CachedMethod())
 				.As<IObjectReturningNewGuids>()
